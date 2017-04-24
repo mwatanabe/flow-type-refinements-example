49583b4853d4a90694c72ee76982ce21e5234de4
mwatanabe <watanabe.seman@gmail.com>
2017-04-24 19:15:21 +0900

    flow example

---
+++ flow-test.js
@@ -0,0 +1,36 @@
+// @flow
+function logString(value: string) {
+    console.log(value);
+}
+
+function logBoolean(value: boolean) {
+    console.log(value);
+}
+
+function simple(value: string | boolean) {
+    if (typeof value === 'string') {
+        logString(value);
+    } else if (typeof value === 'boolean') {
+        logBoolean(value);
+    }
+}
+
+type Action = {
+    type: 'STRING_ACTION',
+    value: string,
+} | {
+    type: 'BOOLEAN_ACTION',
+    value: boolean,
+}
+
+function reduce(state: any, action: Action) {
+    switch (action.type) {
+        case 'STRING_ACTION':
+            otherMethod(action);
+            logString(action.value);
+            break;
+        case 'BOOLEAN_ACTION':
+            logBoolean(action.value);
+            break;
+    }
+}
